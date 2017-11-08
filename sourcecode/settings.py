599de50565576249b278fccfce7da7a06dcb5b09
Rich <pyromaniaclv@gmail.com>
2017-11-08 15:51:56 +0000

    Added score globals

--- sourcecode/settings.py
+++ sourcecode/settings.py
@@ -11,4 +11,8 @@ game_board = [
 
 INDEX = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7}
 VERSION = "v0.1"
-white_turn = True
+
+white_turn = True # Default to true, as white team starts first
+
+white_score = 0 # White team score
+red_score = 0 # Red team score
