ce53814fabfc3f819cbb00525ccb20261ee70e06
Rich <pyromaniaclv@gmail.com>
2017-11-08 14:12:03 +0000

    Added white_turn var
    To keep track of which teams turn it is

--- sourcecode/settings.py
+++ sourcecode/settings.py
@@ -7,7 +7,8 @@ game_board = [
     ["O", " ", "O", " ", "O", " ", "O", " "],
     [" ", "O", " ", "O", " ", "O", " ", "O"],
     ["O", " ", "O", " ", "O", " ", "O", " "]
-]
+] # game_board[Y][X] because 2D arrays ;-;
 
 INDEX = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7}
 VERSION = "v0.1"
+white_turn = True
